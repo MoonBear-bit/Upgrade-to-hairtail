533c34f385804a867a56eeff8bb64c92ea7056f7
MoonBear-bit <dytpql03@naver.com>
2025-07-22 16:22:01 +0900

    Add files via upload

--- code/script.js
+++ code/script.js
@@ -10,6 +10,7 @@ let NameUI=document.getElementById('Name')
 let StoryModeStartButtonUI=document.getElementById('StoryMode')
 let BuyTreatmentKitButtonUI=document.getElementById('BuyTreatmentKit')
 let TreatmentKitUI=document.getElementById('TreatmentKit')
+let RandomMoneyButtonUI=document.getElementById('RandomMoney')
 Fish.style.display='none'
 Water.style.display='none'
 UpgradeButtonUI.style.display='none'
@@ -21,29 +22,37 @@ TryUpgradeUI.style.display='none'
 NameUI.style.display='none'
 BuyTreatmentKitButtonUI.style.display='none'
 TreatmentKitUI.style.display='none'
+RandomMoneyButtonUI.style.display='none'
 let FishLevel=0
 let TryUpgrade=100
 let NeedMoney=100
 let SellMoney=0
-let Money=10000
+let Money=25000
 let TreatmentKit=0
 let Life=0
-let TryUpgradeOfLevel=[100,95,90,90,85,85,80,75,70,65,60,55,50,45,40,35,30,20,15]
-let SellMoneyOfLevel=[0,45,50,500,900,2500,3500,17500,29500,55000,95000,169000,260000,47000,807000,1500000,2900000,4000000,6300000]
+let TryUpgradeOfLevel=[100,98,95,94,90,85,83,80,77,75,73,70,67,65,63,60,57,55,53,50,47,45,43,40,37,35,15,0]
+let SellMoneyOfLevel=[0,45,50,500,900,2500,3500,17500,29500,55000,95000,169000,260000,47000,807000,1500000,2900000,4000000,6300000,11000000,17500000,30500000,50500000,86500000,150000000,250000000,435000000,
+    715500000
+]
 let WhyFailedArray=['교통사고로 인해','수산시장에 잡혀가서','스트레스로','미래를 깨닫고','반1란을 일으켜서','우울증으로','그냥','지1렁이의 심기를 건드려']
-let NameOfLevel=['알','새끼 갈치','어린 갈치','작은 갈치','갈치','큰 갈치','돌갈치','철갈치','은갈치','금갈치','티타늄갈치','다이아몬드갈치','텅스텐갈치','우라늄갈치','오염된 갈치',
-    '유전자가 조작된 갈치','재앙 갈치','늙은 갈치','갈치구이'
+let NameOfLevel=['알','새끼 갈치','어린 갈치','작은 갈치','갈치','큰 갈치','돌갈치','철갈치','은갈치','금갈치','티타늄갈치','다이아몬드갈치','텅스텐갈치','우라늄갈치','유전자 조작 갈치','재앙 갈치','노인 갈치','금품갈치',
+    '반도체 갈치','손질 갈치','냉동 갈치','갈치 구이','되살아난 갈치','총기 갈치','"전투 땅" 갈치','우주로 간 갈치','행성:갈치1972-N','지구:갈치'
 ]
 let i=0
 let a=100
 let b=0
-for (i=0;i<19;i++){
+for (i=0;i<NameOfLevel.length;i++){
     a*=1.7
     a=Math.round(a)
     b+=a
-    console.log(a, b, SellMoneyOfLevel[i],TryUpgradeOfLevel[i],i)
+    console.log(a, b, SellMoneyOfLevel[i],TryUpgradeOfLevel[i],NameOfLevel[i],i)
 }
 //Event
+RandomMoneyButtonUI.addEventListener('click',()=>{
+    let RandomMoney=Math.random()*2
+    Money*=RandomMoney
+    Money=Math.round(Money)
+})
 StoryModeStartButtonUI.addEventListener('click',()=>{
     StoryModeStart()
 })
@@ -73,8 +82,8 @@ SellButtonUI.addEventListener('click',()=>{
     SellMoney=0
 })
 BuyTreatmentKitButtonUI.addEventListener('click',()=>{
-    if (Money>=5000){
-        Money-=5000
+    if (Money>=7500){
+        Money-=7500
         TreatmentKit+=1
     }else{
         alert('돈이 없습니다')
@@ -94,6 +103,7 @@ function StoryModeStart(){
     NameUI.style.display='block'
     BuyTreatmentKitButtonUI.style.display='block'
     TreatmentKitUI.style.display='block'
+    RandomMoneyButtonUI.style.display='block'
 }
 function SuccsessUpgrade(){
     FishLevel+=1
